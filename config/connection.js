419184869b0c2de35b3e757c0b5a4d2420d3c7d3
Xander <xanschwarz@gmail.com>
2021-08-31 14:24:42 -0700

    Added a .env for credential security. Set up connection.js.

---
+++ config/connection.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql2');
+require('dotenv').config();
+
+const connection = mysql.createConnection(
+    {
+      host: 'localhost',
+      port: 3306,
+      user: 'root',
+      password: process.env.DB_PASSWORD,
+      database: 'tracker_db',
+    },
+    console.log(`Connected to the tracker_db database.`)
+  );
+
+module.exports = connection;
--- server.js
+++ server.js
@@ -1,3 +1,2 @@
-const mysql2 = require('mysql2');
 const inquirer = require('inquirer');
 const consoleTable = require('console.table');
